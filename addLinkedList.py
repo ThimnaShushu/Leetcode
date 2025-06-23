9118096b63fe44c0daeb8eae15856e4d68ab2c7d
ThimnaShushu <thimnashushu@gmail.com>
2025-06-23 21:06:06 +0200

    first commit

---
+++ addLinkedList.py
@@ -0,0 +1,26 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = nextclass Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1 = ""
+        num2 = ""
+        while l1:
+            num1 = str(i)+num1
+            l1=l1.next
+            
+        while l2:
+            num2 = str(i)+num2
+            l2=l2.next
+        
+        num3 = int(num1)+ int(num2)
+        num3 = str(num3)
+        l3=[]
+        for i in range(len(num3) -1, -1, -1):
+            l3.append(num3[i])
+            
+        return l3
+    
+print(addTwoNumbers())
+
+        
